8e1194554db532035b4ef42941a4ff3c8b6486be
Nikhil Marathe <nsm.nikhil@gmail.com>
2021-12-01 08:21:13 -0800

    Smarter python solution, as well as fixed koka solution to not fail compile

--- day1/day1.py
+++ day1/day1.py
@@ -2,52 +2,32 @@
 import fileinput
 import functools
 import typing as T
+from itertools import islice
+import collections
 
-class Windows:
-    def __init__(self):
-        self.windows: T.List[T.List[int]] = []
-        self.active_windows = [[], [], []]
-        self.added = 0
+# Smarter
+# from itertools examples
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) -> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
 
-    def rotating_add(self, x: int):
-        self.active_windows[0].append(x)
-        self.active_windows[1].append(x)
-        self.active_windows[2].append(x)
-
-        assert len(self.active_windows[0]) == 3
-        self.windows.append(list(self.active_windows[0]))
-        self.active_windows.pop(0)
-        self.active_windows.append([])
-
-    def add(self, x: int):
-        # some of the starting cases
-        if self.added == 0:
-            self.active_windows[0].append(x)
-        elif self.added == 1:
-            self.active_windows[0].append(x)
-            self.active_windows[1].append(x)
-        else:
-            self.rotating_add(x)
-        self.added += 1 
-
-    def __repr__(self) -> str:
-        return f"{self.windows} ({self.active_windows})"
-
-    def increase_count(self) -> int:
-        summed = [sum(w) for w in self.windows]
-        increase_c = 0
-        def track_inc(a, b):
-            nonlocal increase_c
-            if a < b:
-                increase_c += 1
-            return b
-        functools.reduce(track_inc, summed)
-        return increase_c
-
-windows = Windows()
-for line in fileinput.input():
-    item = int(line)
-    windows.add(item)
-
-print(windows)
-print(windows.increase_count())
+def windowed_increase(l: T.List[str], n: int) -> int:
+    inputs = [int(line) for line in l]
+    windows = [sum(w) for w in sliding_window(inputs, n)]
+    increase_c = 0
+    def track_inc(a, b):
+        nonlocal increase_c
+        if a < b:
+            increase_c += 1
+        return b
+    functools.reduce(track_inc, windows)
+    return increase_c
+lines = list(fileinput.input())
+print(windowed_increase(lines, 1))
+print(windowed_increase(lines, 3))
